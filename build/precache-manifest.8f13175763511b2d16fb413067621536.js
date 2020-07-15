c96375c3c48c5ec9b3fd8b05d4b653662c46af9c
unknown <alexei24@list.ru>
2020-07-15 15:40:03 +0300

    lisätty frontend

---
+++ build/precache-manifest.8f13175763511b2d16fb413067621536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f48b57a4c4898fafd6e5064b665e07a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc2da78435ba94b160f",
+    "url": "/static/css/main.525c2b68.chunk.css"
+  },
+  {
+    "revision": "f9aba95b3f951c008d68",
+    "url": "/static/js/2.c7511e6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7511e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc2da78435ba94b160f",
+    "url": "/static/js/main.fbebd7c4.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
