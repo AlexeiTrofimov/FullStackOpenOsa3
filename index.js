63ebb56f8dd5cabc12c19be4b7bc368cfca407a5
unknown <alexei24@list.ru>
2020-07-16 18:47:15 +0300

    tehtävä 3.22

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
-const express = require('express');
-const { response } = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const { response } = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const app = express()
 const Person = require('./models/person')
 
@@ -16,60 +15,57 @@ app.use(cors())
 
 
 app.get('/info', (request, response) => {
-    var today = new Date();
-    Person.countDocuments({}, (err,result) => {
-      response.send(
-        `<div>
-        <p>Phonebook has info for ${result} people</p> 
-        <p>${today}</p>
-        </div>`
-      )
-    })
-    
+  var today = new Date()
+  Person.countDocuments({}, (err,result) => {
+    response.send(
+      `<div>
+      <p>Phonebook has info for ${result} people</p> 
+      <p>${today}</p>
+      </div>`
+    )
+  })
 })
-  
+
+
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
-  })  
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = Number(request.params.id)
-    Person.findById(request.params.id).then(person => {
-      if (person){
-        response.json(person)
+  Person.findById(request.params.id).then(person => {
+    if (person){
+      response.json(person)
     }
     else{
-        response.status(404).end()
+      response.status(404).end()
     }
 
-    }).catch(error => next(error))
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
+  Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end
     })
     .catch(error => next(error))
-    
 })
 
 app.post('/api/persons', (request,response, next) => {
-    const body = request.body
-    
-    const person = new Person ({
-      name: body.name,
-      number: body.number,
-    })
-    person.save().then(savedPerson => {
-      response.json(savedPerson.toJSON())
-    })
+  const body = request.body
+
+  const person = new Person ({
+    name: body.name,
+    number: body.number,
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON())
+  })
     .catch(error => next(error))
-    
-    
 })
+
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
@@ -108,5 +104,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
